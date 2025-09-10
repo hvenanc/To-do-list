fef747eb924b6354cd26fc7fdb2cd1c59be045e8
hvenanc <hvs.git@gmail.com>
2025-09-10 10:05:01 -0300

    refactor: Renomeação e refatoração dos arquivos to_do_list.py para task.py e refatoração do repositório

--- repository/task_repository.py
+++ repository/task_repository.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 from config.firebase_config import db
-from schemas.to_do_list import ToDoListRequest, ToDoListResponse, helper
+from schemas.task import TaskRequest, TaskResponse, helper
 
 
 class TaskRepository:
@@ -10,23 +10,23 @@ class TaskRepository:
         self.collection = db.collection("to_do_list")
 
 
-    def create(self, task: ToDoListRequest):
+    def create(self, task: TaskRequest) -> TaskResponse:
         doc_ref = self.collection.document(task.id)
         doc_ref.set(task.model_dump())
         return task
 
 
-    def get_all(self):
+    def get_all(self) -> List[TaskResponse]:
         docs = self.collection.stream()
         return [helper(doc) for doc in docs]
     
 
-    def get_by_id(self, id: str):
+    def get_by_id(self, id: str) -> Optional[TaskResponse]:
         doc = self.collection.document(id).get()
         return helper(doc) if doc.exists else None
     
 
-    def update(self, id: str, task: ToDoListRequest):
+    def update(self, id: str, task: TaskRequest) -> Optional[TaskResponse]:
         doc_ref = self.collection.document(id)
         doc_ref.update(task)
         return doc_ref.get()
